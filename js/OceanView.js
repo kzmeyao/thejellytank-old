724af2cfa2ddeaa26ba3e9f69a4fd827c337d17b
kyao <kzmeyao@gmail.com>
2014-06-19 21:30:01 -0700

    logo mouseover animation and media query transition of overlay

--- js/OceanView.js
+++ js/OceanView.js
@@ -3,7 +3,8 @@ var OceanView = Backbone.View.extend({
   events: {
     "click .nav-home" : "goHome",
     "click .nav-works" : "goWork",
-    "click .nav-hello" : "sayHello"
+    "click .nav-hello" : "sayHello",
+    "mouseover .logo"  : "logoHover"
   },
 
   initialize : function() {
@@ -16,10 +17,21 @@ var OceanView = Backbone.View.extend({
     image.onload = function() {
       $(".backgroundsize header").css('background', "url(" + image.src + ") no-repeat center center fixed");
       var $jelly = $("#jelly"),
-        tl = new TimelineLite();
+          tl = new TimelineLite();
       tl.to($jelly, 1, {scale : 2.5})
         .to($jelly, 1, {autoAlpha: 1});
     };
+  },
+
+  logoHover: function(e) {
+    if(!this.hovering) {
+      var that = this;
+      that.hovering = true;
+      var $target = $(e.currentTarget).find(".tentacles");
+      var tl = new TimelineLite();
+      tl.to($target, .7, {y : "-2.5", ease:Circ.easeIn})
+        .to($target, .7, {y : "0", ease:Circ.easeOut, onComplete: function(){that.hovering = false;}});
+    }
   },
 
   goHome : function() {
